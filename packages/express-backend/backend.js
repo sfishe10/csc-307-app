601079a3cd0dbdf4f6960b83800852da6e455759
sfishe10 <sfishe10@calpoly.edu>
2024-04-12 10:22:36 -0700

    search by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,6 +36,10 @@ const findUserByName = (name) => {
         (user) => user["name"] === name
     );
 };
+const findUserById = (id) => {
+    return users["users_list"].find(
+        (user) => user["id"] === id);
+}
 
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -49,6 +53,15 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
     }
 });
 
