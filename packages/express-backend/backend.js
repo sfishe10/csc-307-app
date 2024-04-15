9d0a7248b423f8f544b12f715be771ddd61380ef
sfishe10 <sfishe10@calpoly.edu>
2024-04-15 09:29:06 -0700

    connected frontend to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -58,6 +59,7 @@ const deleteUser = (id) => {
     return false;
 }
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World!");
