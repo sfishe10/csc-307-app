95f7fb13e08135aea89c039ed55044422bfecb3f
sfishe10 <sfishe10@calpoly.edu>
2024-04-26 09:13:17 -0700

    db

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,110 +1,51 @@
 import express from "express";
 import cors from "cors";
+import userServices from "./models/user-services.js"
 
 const app = express();
 const port = 8000;
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-  };
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
-const findUserByNameAndJob = (name, job) => {
-    return users["users_list"].filter(
-        (user) => user["job"] === job && user["name"] === name
-    );
-}
-const findUserById = (id) => {
-    return users["users_list"].find(
-        (user) => user["id"] === id);
-}
-const addUser = (user) => {
-    user.id = Math.random();
-    users["users_list"].push(user);
-    return user;
-};
-const deleteUser = (id) => {
-    const index = users["users_list"].indexOf(findUserById(id));
-    if (index !== -1) {
-        users["users_list"].splice(index, 1);
-        return true;
-    }
-    return false;
-}
 
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
 app.get("/users", (req, res) => {
     const name = req.query.name;
     const job = req.query.job;
-    if (job != undefined && name != undefined) {
-        let result = findUserByNameAndJob(name, job);
-        result = { users_list: result };
-        res.send(result);
-    }
-    else if (job == undefined && name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
+    userServices
+        .getUsers(name, job)
+        .then((result) => {
+            res.send({ users_list: result });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("An error occurred in the server.");
+    });
 });
 app.get("/users/:id", (req, res) => {
     const id = req.params.id;
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send("Resource not found.");
-    } else {
-        res.send(result);
-    }
+    userServices.findUserById(id).then((result) => {
+        if (result === undefined || result === null) {
+            res.status(404).send("Resource not found.");
+        } else res.send({ users_list: result });
+    });
 });
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
-    const added_user = addUser(userToAdd);
-    res.status(201).send(added_user);
-})
+    userServices.addUser(userToAdd).then((saved_user) => {
+        if (saved_user) res.status(201).send(saved_user);
+        else res.status(500).end();
+    });
+});
 app.delete("/users/:id", (req, res) => {
     const userIdToDelete = req.params.id;
-    if (deleteUser(userIdToDelete)) {
-        res.status(204).send();
-    }
-    else {
-        res.status(404).send("Resource not found.");
-    }
-    
-})
+    userServices.deleteUser(userIdToDelete).then((deleted_user) => {
+        if (deleted_user) res.status(204).send(deleted_user);
+        else res.status(404).send("Resource not found.");
+    });
+});
 
 
 app.listen(port, () => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,7 +7,7 @@ function MyApp() {
 
     function removeOneCharacter(index) {
       const person = characters[index];
-      const promise = fetch(`http://localhost:8000/users/${person.id}`, {
+      const promise = fetch(`http://localhost:8000/users/${person._id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
